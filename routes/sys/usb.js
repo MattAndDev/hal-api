78a5863d8635d34aa134659396067e1b15f452dd
Matteo Maronati <me@mattinthe.net>
2017-05-19 03:40:50 +0200

    Up: sys/usb to return also internal usb

--- routes/sys/usb.js
+++ routes/sys/usb.js
@@ -13,8 +13,12 @@ module.exports = function (router, remove) {
   router.route(endpoint).get((req, res) => {
     res.header('Content-Type', 'application/json')
     let usbs = usb.getDeviceList()
-    let external = _.filter(usbs, (usb) => { return usb.deviceAddress > 3 })
-
-    res.send(JSON.stringify(external, null, 2))
+    let external = _.filter(usbs, (usb) => { return usb.deviceAddress >= 4 })
+    let internal = _.filter(usbs, (usb) => { return usb.deviceAddress <= 3 })
+    let data = {
+      external: external,
+      internal: internal
+    }
+    res.send(JSON.stringify(data, null, 2))
   })
 }
