38b179a2229f1f084ecbd9f4ea6e7da3c8b68c92
Matteo Maronati <me@mattinthe.net>
2017-05-23 01:38:03 +0200

    Up: ssh stdout to json

--- routes/sys/ssh.js
+++ routes/sys/ssh.js
@@ -10,10 +10,29 @@ var _ = require('lodash')
 module.exports = function (router, remove) {
   let endpoint = path.relative(process.cwd(), __filename).replace(remove, '').replace('.js', '')
   router.route(endpoint).get((req, res) => {
-    res.header('Content-Type', 'text/plain')
     exec('who', (err, stdout, stderr) => {
-      if (err) throw err
-      res.send(stdout)
+      if (err || stderr) {
+        res.header('Content-Type', 'text/plain')
+        res.send(`${err} \n ${stderr}`)
+        throw err
+      }
+      else {
+        res.header('Content-Type', 'application/json')
+        let stoudArray = stdout.toString().split('\n')
+        let template = ['user', 'terminal', 'date', 'time', 'ip']
+        let results = []
+        _.each(stoudArray, (line, mainIndex) => {
+          let entries = line.split(/\s+/g)
+          let row = {}
+          if (entries.length >= 2) {
+            _.each(entries, (entry, index) => {
+              row[template[index]] = entry
+            })
+            results.push(row)
+          }
+        })
+        res.send(JSON.stringify(results, null, 2))
+      }
     })
   })
 }
