a8be7a7ca2d058f58d3382454c9e4d90a2b07b17
Matteo Maronati <me@mattinthe.net>
2017-05-23 01:18:09 +0200

    Doc: camera/image endpoint

--- routes/camera/image.js
+++ routes/camera/image.js
@@ -1,6 +1,7 @@
 // core
 var path = require('path')
 var exec = require('child_process').exec
+// libs
 var _ = require('lodash')
 // env
 var env = require('./../../.env')
@@ -11,23 +12,31 @@ var env = require('./../../.env')
 module.exports = function (router, remove) {
   let endpoint = path.relative(process.cwd(), __filename).replace(remove, '').replace('.js', '')
   router.route(endpoint).get((req, res) => {
-    let cmd = `raspistill -o ${env.tmpImage} `
+    // base command with output file and --no-preview
+    let cmd = `raspistill -n -o ${env.tmpImage} `
+    // add defaults
     let baseQuery = {
       w: '800',
       h: '600'
     }
+    // if query
     if (req.query) {
+      // merge with defaults
       let customQuery = _.assignIn(baseQuery, req.query)
+      // extend the command
       _.each(customQuery, (value, param) => {
         cmd = cmd + ` -${param} ${value}`
       })
     }
+    // execute
     exec(cmd, (err, stdout, stderr) => {
+      // if any type of error
       if (err || stderr) {
         res.header('Content-Type', 'text/plain')
         res.send(`${err} \n ${stderr}`)
         throw err
       }
+      // everything fine, shipping image
       else {
         res.header('Content-Type', 'image/jpg')
         res.sendFile(env.tmpImage)
