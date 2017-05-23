881dcbd57e07aa181f3114f5f96b7d33bd63a424
Matteo Maronati <me@mattinthe.net>
2017-05-23 03:04:51 +0200

    Up: ssh now restored to promise, providing also tunnels

--- routes/sys/ssh.js
+++ routes/sys/ssh.js
@@ -3,36 +3,56 @@ var path = require('path')
 var exec = require('child_process').exec
 // libs
 var _ = require('lodash')
+// utils
+var stdoutToJSon = require('../../utils/stdout-to-json')
 
 // sys/ssh
 // ============================================
-
+console.log(stdoutToJSon)
 module.exports = function (router, remove) {
+
   let endpoint = path.relative(process.cwd(), __filename).replace(remove, '').replace('.js', '')
   router.route(endpoint).get((req, res) => {
-    exec('who', (err, stdout, stderr) => {
-      if (err || stderr) {
-        res.header('Content-Type', 'text/plain')
-        res.send(`${err} \n ${stderr}`)
-        throw err
-      }
-      else {
-        res.header('Content-Type', 'application/json')
-        let stoudArray = stdout.toString().split('\n')
-        let template = ['user', 'terminal', 'date', 'time', 'ip']
-        let results = []
-        _.each(stoudArray, (line, mainIndex) => {
-          let entries = line.split(/\s+/g)
-          let row = {}
-          if (entries.length >= 2) {
-            _.each(entries, (entry, index) => {
-              row[template[index]] = entry
-            })
-            results.push(row)
-          }
-        })
-        res.send(JSON.stringify(results, null, 2))
-      }
+    // exec who
+    let getSessions = new Promise((resolve, reject) => {
+      let sessions = []
+      exec('who', (err, stdout, stderr) => {
+        if (err || stderr) {
+          stderr ? reject(stderr) : reject(err)
+          throw err
+        }
+        else {
+          let template = ['user', 'terminal', 'date', 'time', 'ip']
+          let sessions = stdoutToJSon(stdout, template)
+          resolve(sessions)
+        }
+      })
     })
+    let getTunnels = new Promise((resolve, reject) => {
+      let tunnels = []
+      exec("lsof -i -n | egrep '\\<ssh\\>'", (err, stdout, stderr) => {
+        if (err || stderr) {
+          stderr ? reject(stderr) : reject(err)
+          throw err
+        }
+        else {
+          let template = ['command', 'pid', 'user', 'boh', 'Ipv', 'bohv2', 'bohv3', 'protocol', 'tunnel', 'status']
+          let tunnels = stdoutToJSon(stdout, template)
+          tunnels = _.remove(tunnels, (o) => {
+            return o.tunnel.indexOf('192.168.0.18') === -1 && o.tunnel.indexOf('*:ssh') === -1
+          })
+          resolve(tunnels)
+        }
+      })
+    })
+    Promise.all([getTunnels, getSessions]).then(values => {
+      let results = {
+        tunnels: values[0],
+        sessions: values[1]
+      }
+      res.header('Content-Type', 'application/json')
+      res.send(JSON.stringify(results, null, 2))
+    })
+
   })
 }
