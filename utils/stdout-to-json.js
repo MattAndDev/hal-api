180d8ceda0601d55e20ce46fe008431de299fdc4
Matteo Maronati <me@mattinthe.net>
2017-05-23 03:22:52 +0200

    Doc: commenting stdout-to-json util

--- utils/stdout-to-json.js
+++ utils/stdout-to-json.js
@@ -6,23 +6,40 @@ var _ = require('lodash')
 
 // stdout to json
 // ============================================
+// parses standard linux stdout to json array of objects
+// as tempalte pass array of header row
+// or pass true f the stdout contains a header row
+//
+// @param table - string
+// @param template - array/boolean
+//
 
 module.exports = function (table, template) {
+  // scaffolging
   let jsonTable = []
+  // cleanup and first split
   table = table.toString().replace(/(\r\n|\r|\n)/g, '\n').split('\n')
+  // if header row is in table manipulate vars
   if (typeof template === "boolean" && template === true) {
     template = table[0].split(/\s+/g)
     table = _.remove(table, (o, i) => { return i !== 0 })
   }
+  // go trough the lines
   _.each(table, (line, mainIndex) => {
-    let entries = line.split(/\s+/g)
+    // scaffolding
     let row = {}
+    // create entries
+    let entries = line.split(/\s+/g)
+    // be sure to get something
     if (entries.length >= 2) {
+      // map entry to template
       _.each(entries, (entry, index) => {
         row[template[index]] = entry
       })
+      // push the row
       jsonTable.push(row)
     }
   })
+  // return the object
   return jsonTable
 }
