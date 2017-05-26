7fcdfa78fa308768d637903c4086534eddee1242
Matteo Maronati <me@mattinthe.net>
2017-05-26 17:16:57 +0200

    Up: added custom winston-logger instance, setup transports, initial tests #8

---
+++ utils/winston-logger.js
@@ -0,0 +1,32 @@
+// core
+var path = require('path')
+// libs
+var _ = require('lodash')
+var winston = require('winston')
+// env
+var env = require('.env')
+
+// winston logger magic
+// ============================================
+
+module.exports = new (winston.Logger)({
+  transports: [
+    new (winston.transports.File)({
+      name: 'debug',
+      filename: path.resolve(env.logs.debug),
+      level: 'debug'
+    }),
+    new (winston.transports.File)({
+      name: 'error',
+      handleExceptions: true,
+      filename: path.resolve(env.logs.error),
+      level: 'error'
+    }),
+    new (winston.transports.Console)({
+      name: 'debug-console',
+      colorize: true,
+      level: 'debug'
+    })
+  ],
+  exitOnError: false
+})
