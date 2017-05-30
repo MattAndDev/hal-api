c4ad331b580f4464e1b214ae75838b3ce5a5e85d
Matteo Maronati <me@mattinthe.net>
2017-05-31 01:49:45 +0200

    Up: adding sme first basic testing (#11)

--- index.js
+++ index.js
@@ -11,43 +11,48 @@ var wLog = require('utils/winston-logger')
 var env = require('.env')
 
 
-class Api {
+module.exports = class Api {
 
   constructor () {
     this.api = new Express()
     this.router = Express.Router()
     this.api.use('/', this.router)
     this.routes = {}
+    this.init()
   }
 
   init () {
     wLog.log('debug', 'hal-api initalised')
 
-    this.api.get('/', function (req, res) {
+    this.api.get('/', (req, res) => {
       res.send('HAL Rasp-pi api.')
     })
 
-    this.api.listen(env.port, function () {
+    this.api.listen(env.port, () => {
       wLog.log('debug', `hal-api listening on port ${env.port}`)
     })
 
-    this._buildRoutes()
+    this._buildRoutes('./routes/**/*.js', (err, routes) => {
+      if (!err) {
+        wLog.log('debug', 'mounted routes')
+      }
+    })
   }
 
-  _buildRoutes () {
-    glob('./routes/**/*.js', (err, routes) => {
+  _buildRoutes (routes, cb) {
+    glob(routes, (err, routes) => {
       if (!err) {
         _.each(routes, (route) => {
           this.routes[route] = require(route)
           this.routes[route](this.router, 'routes')
         })
+        cb(null, this.routes)
       }
       else {
         wLog.log('debug', 'Api._buildRoutes failed building routes', {err: err})
+        cb(err)
       }
     })
+    return false
   }
 }
-
-let api = new Api()
-api.init()
--- test/index.js
+++ test/index.js
@@ -1,11 +1,19 @@
 var expect = require('chai').expect
-var Api = require('../index')
+var Hal = require('../index')
 
-describe('Api module', function () {
-  it('initalises routes', function * () {
-    var test = () => {
-      return false
-    }
-    expect(test).to.eql(true)
+
+describe('Api', () => {
+  describe('_routeBuilder', () => {
+    let hal = new Hal()
+    it('routes built', (done) => {
+      let routes = hal._buildRoutes('./routes/**/*.js', (err, routes) => {
+        expect(routes).to.be.an.instanceof(Object)
+        expect(err).to.equal(null)
+        done()
+      })
+    })
+    it('has endpoints', () => {
+      expect(hal.router.stack).to.be.an.instanceof(Array)
+    })
   })
 })
