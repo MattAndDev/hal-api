2a39dec10386e11590a471d7b4959deb6e31db03
Matteo Maronati <me@mattinthe.net>
2017-05-30 20:15:22 +0200

    Up: setup testing (#11)

---
+++ test/index.js
@@ -0,0 +1,11 @@
+var expect = require('chai').expect
+var Api = require('../index')
+
+describe('Api module', function () {
+  it('initalises routes', function * () {
+    var test = () => {
+      return false
+    }
+    expect(test).to.eql(true)
+  })
+})
